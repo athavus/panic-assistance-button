492061d45998bab5a6d84193aa7ba2b15bd4eeb4
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2025-07-30 01:10:00 -0300

    commit inicial

---
+++ src/main/java/com/alerta/botao_panico/BotaoPanicoApplication.java
@@ -0,0 +1,13 @@
+package com.alerta.botao_panico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotaoPanicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotaoPanicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alerta/botao_panico/BotaoPanicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alerta.botao_panico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotaoPanicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
